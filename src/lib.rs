ece21556e9a6b360d5cd92d8ea70b32448229ac3
noyobo <noyobo@gmail.com>
2025-03-18 22:39:39 +0800

    println

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,28 @@
 #[macro_use]
 extern crate napi_derive;
 
+use napi::bindgen_prelude::Undefined;
+use lightningcss::stylesheet::{ParserOptions, PrinterOptions, StyleSheet};
+
 #[napi]
-pub fn style_factory(css: String) -> String {
-  css
+pub fn style_factory(css: String) -> Undefined {
+    let stylesheet = StyleSheet::parse(&css, ParserOptions::default()).unwrap();
+    let res = stylesheet
+        .to_css(PrinterOptions::default())
+        .unwrap();
+    println!("{}", res.code);
+    ()
 }
 
 #[cfg(test)]
 mod tests {
-  use super::style_factory;
+    use super::*;
 
-  #[test]
-  fn test_style_factory() {
-    assert_eq!(style_factory("body { color: red; }".to_string()), "body { color: red; }");
-  }
+    #[test]
+    fn test_style_factory() {
+        assert!(std::panic::catch_unwind(|| {
+            style_factory("body { color: red; }".to_string());
+        })
+        .is_ok());
+    }
 }
