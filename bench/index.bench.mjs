87ad8da6e26c4bb86db1b9e22f644700743d4207
子长 <zichang.nong@tuya.com>
2025-03-19 10:53:55 +0800

    Update index.bench.mjs

--- bench/index.bench.mjs
+++ bench/index.bench.mjs
@@ -4,7 +4,7 @@ import { bench } from 'vitest';
 
 import styleFactoryPkg from 'style-factory/package.json';
 
-const css = `body { color: red; height: 100px; width: 100px; }`;
+const css = `body { color: red; height: 100px; width: 100rpx; }`;
 
 bench('rust', () => {
   rustStyleFactory(css);
